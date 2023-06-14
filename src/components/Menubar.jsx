6f6bcbfcb24c4ba68d54b522135efc8c4d8032c9
sharifmrahat <sharifmrahat@gmail.com>
2023-06-14 20:39:09 +0600

    project setup is done

---
+++ src/components/Menubar.jsx
@@ -0,0 +1,23 @@
+function Menubar() {
+  return (
+    <div className="m-4 lg:m-8">
+      <div className="w-full flex flex-col justify-center items-center">
+        <h1 className="text-4xl text-green-800 font-semibold mb-4">
+          Farm Widen
+        </h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla quasi
+          reprehenderit assumenda quibusdam unde animi sapiente praesentium ad
+          nemo corporis.
+        </p>
+        <h1 className="text-2xl text-blue-800 font-semibold">
+          FGC Science Club
+        </h1>
+
+        <div className="">Important Links</div>
+      </div>
+    </div>
+  );
+}
+
+export default Menubar;
